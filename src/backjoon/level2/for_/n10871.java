d181766398b36653ac74c82e972e04311730e6ea
limdotori <89769868+limdotori@users.noreply.github.com>
2021-09-06 09:07:28 +0900

    weekend

---
+++ src/backjoon/level2/for_/n10871.java
@@ -0,0 +1,19 @@
+package backjoon.level2.for_;
+
+import java.util.Scanner;
+
+public class n10871 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	int N = scan.nextInt();
+	int X = scan.nextInt();
+	
+	int arr[]=new int[N];
+	
+	
+	
+	
+	
+}
+}
---
+++ src/backjoon/level2/for_/n2439.java
@@ -0,0 +1,22 @@
+package backjoon.level2.for_;
+
+import java.util.Scanner;
+
+public class n2439 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	int N = scan.nextInt();
+	
+	for(int i=1;i<=N;i++) {
+		for(int j=1;j<=N-i;j++) {
+			System.out.print(" ");
+		}
+		for(int k=1;k<=i;k++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+	scan.close();
+	}
+}
