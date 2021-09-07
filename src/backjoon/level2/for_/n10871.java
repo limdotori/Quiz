6dd4828bbd6891a91a3134da9769c49f149b26be
limdotori <89769868+limdotori@users.noreply.github.com>
2021-09-07 11:30:57 +0900

    persnal study

--- src/backjoon/level2/for_/n10871.java
+++ src/backjoon/level2/for_/n10871.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class n10871 {
 public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	
-	int N = scan.nextInt();
-	int X = scan.nextInt();
-	
-	int arr[]=new int[N];
-	
-	
-	
-	
-	
-}
-}
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		int X = scan.nextInt();
+		
+		int [] arr = new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i]=scan.nextInt();
+			}
+		for(int i=0;i<N;i++) {
+			if(arr[i]<X) {
+				System.out.print(arr[i]+" ");
+				}
+			}
+		scan.close();	
+		}
+	}
---
+++ src/backjoon/level2/while_/n10952.java
@@ -0,0 +1,19 @@
+package backjoon.level2.while_;
+
+import java.util.Scanner;
+
+public class n10952 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	while(true) {
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+			if(a==0&&b==0) {
+				scan.close();
+				break;
+			}
+			System.out.println(a+b);	
+		}
+	}
+}
