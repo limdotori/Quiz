5b0d345b377f46878302d94672d52e4095529c2c
limdotori <89769868+limdotori@users.noreply.github.com>
2021-09-14 09:20:39 +0900

    20210913

--- src/backjoon/level4/n10951.java
+++ src/backjoon/level4/n10951.java
@@ -1,5 +1,18 @@
 package backjoon.level4;
 
-public class n10951 {
+import java.util.Scanner;
 
+public class n10951 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+
+	while(scan.hasNextInt()) {
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.println(a+b);
+	}
+	
+	
+}
 }
